fc2e612a1b7a4cf33f055d3b06e984f7b6b0c377
Parkky <parth.kale.dev@gmail.com>
2026-04-25 22:01:15 +0530

    recator: relacted the open agent

---
+++ helpers/openagent.py
@@ -0,0 +1,73 @@
+import logging
+from dotenv import load_dotenv
+from livekit import rtc
+from livekit.agents import (
+    Agent,
+    AgentServer,
+    AgentSession,
+    JobContext,
+    JobProcess,
+    cli,
+    inference,
+    room_io,
+)
+from livekit.plugins import (
+    noise_cancellation,
+    silero,
+)
+from livekit.plugins.turn_detector.multilingual import MultilingualModel
+
+logger = logging.getLogger("agent-Sage-1388")
+
+load_dotenv(".env.local")
+
+
+class DefaultAgent(Agent):
+    def __init__(self) -> None:
+        super().__init__(
+            instructions="""
+Your name is Karan. You are a voice AI assistant. You are talking to a user. 
+""",
+        )
+
+    async def on_enter(self):
+        await self.session.generate_reply(
+            instructions="""Greet the user and offer your assistance.""",
+            allow_interruptions=True,
+        )
+
+
+server = AgentServer()
+
+def prewarm(proc: JobProcess):
+    proc.userdata["vad"] = silero.VAD.load()
+
+server.setup_fnc = prewarm
+
+async def entrypoint(ctx: JobContext):
+    session = AgentSession(
+        stt=inference.STT(model="deepgram/nova-3", language="en"),
+        llm=inference.LLM(model="openai/gpt-4.1-mini"),
+        tts=inference.TTS(
+            model="cartesia/sonic-3",
+            voice="9626c31c-bec5-4cca-baa8-f8ba9e84c8bc",
+            language="en"
+        ),
+        turn_detection=MultilingualModel(),
+        vad=ctx.proc.userdata["vad"],
+        preemptive_generation=True,
+    )
+
+    await session.start(
+        agent=DefaultAgent(),
+        room=ctx.room,
+        room_options=room_io.RoomOptions(
+            audio_input=room_io.AudioInputOptions(
+                noise_cancellation=lambda params: noise_cancellation.BVCTelephony() if params.participant.kind == rtc.ParticipantKind.PARTICIPANT_KIND_SIP else noise_cancellation.BVC(),
+            ),
+        ),
+    )
+
+
+if __name__ == "__main__":
+    cli.run_app(server)
